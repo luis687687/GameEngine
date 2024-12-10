c91a53a860187492762733f8a29a0a0dde8f0625
luis687687 <luismarque687@gmail.com>
2024-12-10 10:46:46 +0100

    updates

--- scripts/GameObjects/HUD/Area/Back.js
+++ scripts/GameObjects/HUD/Area/Back.js
@@ -2,7 +2,6 @@ import Area from "./Area.js";
 
 export default class Back extends Area {
   constructor(game){
-    super(game, "../copy/sprites/hud/hud_bg.png", 285, 98, 255, 110)
-    
+    super(game, "./sprites/hud/hud_bg.png", 285, 98, 255, 110)
   }
 }
